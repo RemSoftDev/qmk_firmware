835a15fa9d8da401517f248ccd3576eeaf3628e5
Taras Derkach <derkachtf@gmail.com>
2019-09-06 00:00:15 +0300

    interrogation of a matrix and motors test ok

--- keyboards/2claws/brain/keymaps/default/keymap.c
+++ keyboards/2claws/brain/keymaps/default/keymap.c
@@ -59,15 +59,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		//			KC_5,   MO(_FN),  KC_P7,   KC_P8,    KC_LEFT, KC_UP,   KC_RGHT, KC_DOWN,           KC_ENT,  KC_CAPS,           KC_P9,    KC_SLSH, KC_DOT,  KC_COMM, KC_M,    KC_N,    KC_RCTL,    KC_PMNS
 	*/
 	[_QWERTY] = LAYOUT(
-			RGB_TOG, \
-			KC_F1,  KC_F2,    KC_F1,    KC_F1,   KC_F5,   KC_F6,                                                                              KC_2,    KC_3,    KC_4,    KC_5,    KC_F1,  KC_F1, \
-			KC_1,   KC_F1,   KC_F1,   KC_F1,    KC_F1,    KC_3,    KC_4,    KC_5,    KC_F1,  KC_F1, KC_E,    KC_F1,   KC_F1,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_F1,  KC_F1, \
-			KC_2,   KC_F1,   KC_F1,  KC_F1,    KC_F1,    KC_R,    KC_T,    KC_B,    KC_F1,  KC_F1, KC_P,    KC_F1,   KC_F1,  KC_0,    KC_9,    KC_8,    KC_7,    KC_6,    KC_F1,  KC_F1,\
-			KC_3,   KC_F1,   KC_F1,  KC_F1,    KC_F1,    KC_D,    KC_F,    KC_G,    KC_F1,  KC_F1, KC_P,    KC_F1,  KC_F1,  KC_P,    KC_F1,    KC_I,    KC_U,    KC_Y,    KC_F1,   KC_F1,\
-			KC_4,   KC_F1,  KC_F1,   KC_F1,    KC_F1,    KC_X,    KC_C,    KC_V,    KC_B,     KC_F1, KC_F1, KC_F1,   KC_F1,  KC_F1, KC_F1,    KC_K,    KC_J,    KC_H,    KC_F1,   KC_F1,\
-			KC_5,   KC_F1,  KC_F1,   KC_F1,    KC_F1, KC_F1,   KC_F1, KC_F1,           KC_F1,  KC_F1,           KC_F1,    KC_F1, KC_F1,  KC_F1, KC_M,    KC_N,    KC_F1,    KC_F1
+RGB_TOG, \
+\
+	KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                                        \
+	KC_P1,   KC_PAST,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,  KC_INS,  \
+	KC_P2,   KC_PSLS,  KC_P9,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_PSCR,  KC_NLCK, \
+	KC_P3,   KC_APP,   KC_P8,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_UP,    KC_PPLS, \
+	KC_P4,   KC_LCTL,  KC_P7,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PENT,  KC_END,  \
+	KC_LCTL, KC_LALT,  KC_P6,   KC_P5,   KC_SPC,  KC_ESC,  KC_TAB,  KC_CAPS,           KC_PDOT, \
+\
+								   								 KC_F7,    KC_F8,   KC_F9,    KC_F10,    KC_F11,   KC_F12,  \
+							KC_HOME, KC_SLCK,  KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_MINS,   KC_EQL,   KC_NUBS,  \
+							KC_PGUP, KC_BRK,   KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,     KC_LBRC,   KC_RBRC,  KC_F13, \
+							KC_P3,   KC_PMNS,   KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_PCMM,   KC_NUHS,  KC_F14, \
+							KC_P4,   KC_PPLS,  KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_BSLS,  KC_DEL,    KC_SLSH,  KC_F15,  \
+							KC_DOWN,           KC_LALT, KC_LEFT, KC_UP,    KC_RGHT, KC_COMM,  KC_PGUP,   KC_RALT,  MO(_FN)
 		),
-
 	[_FN] = LAYOUT(
 			KC_TRNS, \
 			KC_TRNS,   KC_TRNS,   KC_F3,    KC_F4,   KC_F5,   KC_F6,                                                                               KC_2,    KC_3,    KC_4,    KC_5,    KC_PGDN,   KC_PGUP, \
@@ -140,6 +147,28 @@ void D1_blink(uint16_t takt) {
 
     } 
 }
+
+
+void slave_motor_blink(uint16_t takt) {
+	static uint16_t d1_timer = 0;
+	static bool is_d1 = false;
+
+    if (timer_elapsed(d1_timer) > takt) {
+		d1_timer = timer_read();
+		if (is_d1) {
+			is_d1 = false;
+			//D1_off();
+			slave_motor_off(0x30, 1);
+		}
+		else {
+			is_d1 = true;
+			//D1_on();
+			slave_motor_on(0x30, 1);//slave_motor_off(uint8_t slave_i2c_addr, uint8_t motor_number)
+		}
+
+    }
+}
+
 
 // в самом начале инициалицации
 void keyboard_pre_init_user(void) {
@@ -359,7 +388,7 @@ uint16_t test_timer =0;
 void matrix_scan_user(void) {     //# The very important timer. 
 
 //	_delay_ms(20); // гипотеза что слишком частій опрос слева вешает его
-	D1_blink(1000);
+//	D1_blink(1000);
     if (timer_elapsed(test_timer) > 2000) {
 		test_timer = timer_read();
 		slaves_motors_chek();
