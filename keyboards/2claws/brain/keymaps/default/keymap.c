ba3d4f381f84d61c678e60a982754bab5f26d1ba
oleksandr.dubyna <struggleendlessly@hotmail.com>
2019-10-20 14:19:05 -0400

    v

--- keyboards/2claws/brain/keymaps/default/keymap.c
+++ keyboards/2claws/brain/keymaps/default/keymap.c
@@ -36,7 +36,8 @@ enum {
   TD_SHIFT_CAPS,
   TD_ALT_ENTER,
   TD_F2_PLUS_ENTER,
-  TD_F5__CTRL_K_D
+  TD_F5__CTRL_K_D,
+  TD_CTRL_S__CTRL_SHIFT_S
 };
 
 void F2_PLUS_ENTER(qk_tap_dance_state_t *state, void *user_data) {
@@ -51,22 +52,23 @@ void F5__CTRL_K_D(qk_tap_dance_state_t *state, void *user_data) {
     if (state->count == 1) {
         tap_code(KC_F5);
     } else {
-        register_code(KC_LCTRL);
-        register_code(KC_K);
-        register_code(KC_D);
-        unregister_code(KC_D);
-        unregister_code(KC_K);
-        unregister_code(KC_LCTRL);
+        register_code(KC_LSHIFT);
+        register_code(KC_LALT);
+        register_code(KC_F);
+        unregister_code(KC_F);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSHIFT);
     }
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_PSCREEN_CAD]   = ACTION_TAP_DANCE_DOUBLE(KC_PSCREEN, LCTL(LALT(KC_DEL))),
-    [TD_PAUSE_WINL]    = ACTION_TAP_DANCE_DOUBLE(KC_PAUSE, LWIN(KC_L)),
-    [TD_SHIFT_CAPS]    = ACTION_TAP_DANCE_DOUBLE(KC_LSHIFT, KC_CAPSLOCK),
-    [TD_ALT_ENTER]     = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_ENTER),
-    [TD_F2_PLUS_ENTER] = ACTION_TAP_DANCE_FN (F2_PLUS_ENTER),
-    [TD_F5__CTRL_K_D]  = ACTION_TAP_DANCE_FN (F5__CTRL_K_D)
+    [TD_PSCREEN_CAD]          = ACTION_TAP_DANCE_DOUBLE(KC_PSCREEN, LCTL(LALT(KC_DEL))),
+    [TD_PAUSE_WINL]           = ACTION_TAP_DANCE_DOUBLE(KC_PAUSE, LWIN(KC_L)),
+    [TD_SHIFT_CAPS]           = ACTION_TAP_DANCE_DOUBLE(KC_LSHIFT, KC_CAPSLOCK),
+    [TD_ALT_ENTER]            = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_ENTER),
+    [TD_F2_PLUS_ENTER]        = ACTION_TAP_DANCE_FN (F2_PLUS_ENTER),
+    [TD_F5__CTRL_K_D]         = ACTION_TAP_DANCE_FN (F5__CTRL_K_D),
+    [TD_CTRL_S__CTRL_SHIFT_S] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_S), LCTL(LSFT(KC_S)))
 };
 
 enum custom_keycodes {
@@ -160,9 +162,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* brain key */	 HWTEST, \
 /*		 	0				1	    	    2			        3		                4		 5		  6		    7			8			9		*/
 /* f */	 KC_F1,         KC_F2,            KC_F3,                KC_F4,                 KC_F5,   KC_F6,
-/* 0 */	 KC_NO,         KC_NO,            KC_GRAVE,             KC_1,                  KC_2,    KC_3,    KC_4,      KC_5,     KC_PGUP,   KC_PGDOWN, \
+/* 0 */	 KC_NO,         KC_ESCAPE,        KC_GRAVE,             KC_1,                  KC_2,    KC_3,    KC_4,      KC_5,     KC_PGUP,   KC_PGDOWN, \
 /* 1 */	 TO(_FN_CSGO),  TG(_FN_NUM),      KC_RBRACKET,          KC_W,                  KC_E,    KC_R,    KC_T,      KC_B,     KC_ENTER,  TD(TD_PSCREEN_CAD), \
-/* 2 */	 TO(_FN_EML),   KC_LCTRL,         KC_LBRACKET,          KC_A,                  KC_S,    KC_D,    KC_F,      KC_G,     KC_LWIN,   KC_ESCAPE, \
+/* 2 */	 TO(_FN_EML),   KC_LCTRL,         KC_LBRACKET,          KC_A,                  KC_S,    KC_D,    KC_F,      KC_G,     KC_LWIN,   TD(TD_CTRL_S__CTRL_SHIFT_S), \
 /* 3 */	 TO(_FN_PSWD),  TD(TD_ALT_ENTER), TD(TD_SHIFT_CAPS),    KC_Q,                  KC_Z,    KC_X,    KC_C,      KC_V,     KC_SPACE,  KC_BSPACE, \
 /* 4 */	 TO(_FN_DEV),   KC_TAB,           TD(TD_F2_PLUS_ENTER), TD(TD_F5__CTRL_K_D),   KC_LEFT, KC_UP,   KC_RIGHT,  KC_SPACE,            KC_DOWN, \
 /*		    0					1			2		3	    	4		 5		  6	    	7			8	        	9		*/
@@ -179,14 +181,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*		 	0	    	1	        2		   3		      4		   5	    	6		  7			8			9		*/
 /* f */	 KC_TRNS,  KC_TRNS,       KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,
 /* 0 */	 KC_TRNS,  KC_NUMLOCK,    KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS, \
-/* 1 */	 KC_TRNS,  TG(_FN_NUM),   KC_PSLS,   KC_KP_MINUS,  KC_KP_9,  KC_KP_8,   KC_KP_7,   KC_KP_0,  KC_TRNS,  KC_TRNS, \
+/* 1 */	 KC_TRNS,  TO(_BASE),     KC_PSLS,   KC_KP_MINUS,  KC_KP_9,  KC_KP_8,   KC_KP_7,   KC_KP_0,  KC_TRNS,  KC_TRNS, \
 /* 2 */	 KC_TRNS,  KC_TRNS,       KC_PAST,   KC_KP_PLUS,   KC_KP_6,  KC_KP_5,   KC_KP_4,   KC_KP_0,  KC_TRNS,  KC_TRNS, \
 /* 3 */	 KC_TRNS,  KC_TRNS,       KC_TRNS,   KC_KP_DOT,    KC_KP_3,  KC_KP_2,   KC_KP_1,   KC_KP_0,  KC_TRNS,  KC_TRNS, \
 /* 4 */	 KC_TRNS,  KC_TRNS,       KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,            KC_TRNS, \
 /*		    0			1		2		  3	    	4	       5		  6	          	7			8	        	9		*/
 /* f */							   			      KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS, \
 /* 5 */	 KC_TRNS,  	KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_NUMLOCK,   KC_TRNS, \
-/* 6 */	 KC_TRNS,   KC_TRNS,  KC_KP_0,  KC_KP_7,  KC_KP_8,  KC_KP_9,   KC_KP_MINUS,  KC_PSLS,    TG(_FN_NUM),  KC_TRNS, \
+/* 6 */	 KC_TRNS,   KC_TRNS,  KC_KP_0,  KC_KP_7,  KC_KP_8,  KC_KP_9,   KC_KP_MINUS,  KC_PSLS,    TO(_BASE),    KC_TRNS, \
 /* 7 */	 KC_TRNS,   KC_TRNS,  KC_KP_0,  KC_KP_4,  KC_KP_5,  KC_KP_6,   KC_KP_PLUS,   KC_PAST,    KC_TRNS,      KC_TRNS, \
 /* 8 */	 KC_TRNS,   KC_TRNS,  KC_KP_0,  KC_KP_1,  KC_KP_2,  KC_KP_3,   KC_KP_DOT,    KC_TRNS,    KC_TRNS,      KC_TRNS, \
 /* 9 */	 KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS
